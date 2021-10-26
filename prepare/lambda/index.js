8195282df0b3bba8cc1df1204d4fcd03c22e6b27
kyutae <ken2484@gmail.com>
2021-10-26 22:22:07 +0900

    람다 샘플 테스트

---
+++ prepare/lambda/index.js
@@ -0,0 +1,32 @@
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
+
+const s3 = new AWS.S3();
+
+exports.handler = async (event, context, callback) => {
+  const Bucket = event.Records[0].s3.bucket.name;
+  const Key = decodeURIComponent(event.Records[0].s3.object.key);
+  console.log(Bucket, Key);
+  const filename = Key.split('/')[Key.split('/').length - 1];
+  const ext = Key.split('.')[Key.split('.').length - 1].toLowerCase();
+  const requireFormat = ext === 'jpg' ? 'jpeg' : ext;
+  console.log('filename', filename, 'ext', ext);
+
+  try {
+    const s3Object = await s3.getObject({ Bucket, Key }).promise();
+    console.log('original', s3Object.Body.length);
+    const resizedImage = await sharp(s3Object.Body)
+      .resize(200, 200, { fit: 'inside'})
+      .toFormat(requireFormat)
+      .toBuffer();
+    await s3.putObject({
+      Bucket,
+      Key: `thump/${filename}`
+    });
+    console.log('put', resizedImage.length);
+    return callback(null, `thumb/${filename}`);
+  } catch(error) {
+    console.error(error);
+    return callback(error);
+  }
+}
